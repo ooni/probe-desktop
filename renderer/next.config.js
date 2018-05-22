d1b4730469c2fcf111408d658c4c0534aa9aff0f
Arturo Filastò <arturo@filasto.net>
2018-05-22 12:45:42 -0700

    Upgrade to next.js 6.0.3 and babel 7 :rage:

--- renderer/next.config.js
+++ renderer/next.config.js
@@ -1,10 +1,15 @@
 module.exports = {
-  webpack(config) {
+  webpack: (config) => {
     config.target = 'electron-renderer'
+
     config.node = {
       __dirname: false,
       __filename: false
     }
+
+    config.externals = config.externals || {}
+    config.externals['react'] = 'React'
+
     config.module.rules.push({
       test: /\.(eot|ttf|woff|woff2|otf)$/,
       use: [
@@ -15,7 +20,8 @@ module.exports = {
     })
     return config
   },
-  exportPathMap() {
+
+  exportPathMap: () => {
     // Let Next.js know where to find the entry page
     // when it's exporting the static bundle for the use
     // in the production version of your app
