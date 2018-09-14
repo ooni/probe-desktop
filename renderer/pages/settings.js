c3e262b6739a0de728e36195c710c199e74115c7
Arturo Filastò <arturo@filasto.net>
2018-09-14 18:58:19 +0200

    Implement basic settings page

--- renderer/pages/settings.js
+++ renderer/pages/settings.js
@@ -1,24 +1,91 @@
 import electron from 'electron'
 import React from 'react'
 
+import styled from 'styled-components'
 
 import Layout from '../components/Layout'
 import Sidebar from '../components/Sidebar'
 
+import BackButton from '../components/BackButton'
+
 import {
+  Flex,
+  Box,
   Heading,
+  Container,
+  Label,
+  Checkbox,
   Text
 } from 'ooni-components'
+
+const TopBar = styled.div`
+  background-color: ${props => props.theme.colors.blue5};
+  color: ${props => props.theme.colors.white};
+  width: 100%;
+  padding-top: 20px;
+  padding-bottom: 20px;
+`
+
+const getConfigValue = (config, optionKey) => optionKey.split('.').reduce((o,i) => o[i] || false, config)
+
+class BooleanOption extends React.Component {
+  constructor(props) {
+    super(props)
+    this.handleChange = this.handleChange.bind(this)
+  }
+
+  handleChange(event) {
+    const {
+      optionKey,
+      config
+    } = this.props
+
+    const remote = electron.remote
+    const { setConfig } = remote.require('./utils/config')
+
+    const target = event.target
+    const newValue = target.type === 'checkbox' ? target.checked : target.value
+    const oldValue = getConfigValue(config, optionKey)
+    setConfig(optionKey, oldValue, newValue).then(() => {
+      this.props.onConfigSet()
+    }).catch(() => {
+      console.log('inconsistency detected')
+      this.props.onConfigSet()
+    })
+  }
+
+  render() {
+    const {
+      label,
+      optionKey,
+      config
+    } = this.props
+
+
+    if (config === null) {
+      return <div />
+    }
+
+    const checked = getConfigValue(config, optionKey)
+    return (
+      <Label>
+        <Checkbox checked={checked} onChange={this.handleChange} />
+        {label}
+      </Label>
+    )
+  }
+}
 
 class Settings extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      config: {}
+      config: null
     }
+    this.reloadConfig = this.reloadConfig.bind(this)
   }
 
-  componentDidMount() {
+  reloadConfig() {
     const remote = electron.remote
     const { getConfig } = remote.require('./utils/config')
 
@@ -27,6 +94,10 @@ class Settings extends React.Component {
         config
       })
     })
+  }
+
+  componentDidMount() {
+    this.reloadConfig()
   }
 
 
@@ -38,10 +109,45 @@ class Settings extends React.Component {
     return (
       <Layout>
         <Sidebar>
-          <Heading h={3}>Config dump</Heading>
-          <Text>
-            {JSON.stringify(config, null, ' ')}
-          </Text>
+          <Box w={1}>
+            <TopBar>
+              <Flex align='center'>
+                <Box>
+                  <BackButton />
+                </Box>
+                <Box>
+                  <Heading h={3}>Settings</Heading>
+                </Box>
+              </Flex>
+            </TopBar>
+            <Container pt={3}>
+              <Heading h={4}>Sharing</Heading>
+              <BooleanOption
+                onConfigSet={this.reloadConfig}
+                label='Publish results'
+                optionKey='sharing.upload_results'
+                config={config} />
+
+              <BooleanOption
+                onConfigSet={this.reloadConfig}
+                label='Include network information'
+                optionKey='sharing.include_asn'
+                config={config} />
+
+              <BooleanOption
+                onConfigSet={this.reloadConfig}
+                label='Include country information'
+                optionKey='sharing.include_country'
+                config={config} />
+
+              <BooleanOption
+                onConfigSet={this.reloadConfig}
+                label='Include my IP address'
+                optionKey='sharing.include_ip'
+                config={config} />
+
+            </Container>
+          </Box>
         </Sidebar>
       </Layout>
     )
