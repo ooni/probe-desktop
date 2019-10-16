6a120596acb19a06efea3797df31545909ea7cd9
Sarath M S <sarath.ms@gmail.com>
2019-10-16 14:28:38 -0400

    Assign default props to StatusBox

--- renderer/components/measurement/StatusBox.js
+++ renderer/components/measurement/StatusBox.js
@@ -4,7 +4,7 @@ import { Flex, Box, Text } from 'ooni-components'
 
 import { colorMap } from './MeasurementContainer'
 
-const StatusBox = ({label, value, ok}) => (
+const StatusBox = ({label, value, ok = 'true'}) => (
   <Flex flexWrap='wrap'>
     <Box width={1}>
       <Text fontSize={1}>{label}</Text>
@@ -18,7 +18,7 @@ const StatusBox = ({label, value, ok}) => (
 StatusBox.propTypes = {
   label: PropTypes.node.isRequired,
   value: PropTypes.node.isRequired,
-  ok: PropTypes.bool.isRequired
+  ok: PropTypes.bool
 }
 
 export default StatusBox
