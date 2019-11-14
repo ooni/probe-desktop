780baf70abddafacbb16d75bdde6e2c8196e07c0
Arturo Filastò <arturo@filasto.net>
2019-11-14 09:49:13 +0100

    Improvements to the macOS notarize process

--- scripts/notarize.js
+++ scripts/notarize.js
@@ -1,18 +1,24 @@
-require('dotenv').config();
-const { notarize } = require('electron-notarize');
+require('dotenv').config()
+const pkg = require('../package.json')
+const { notarize } = require('electron-notarize')
 
 exports.default = async function notarizing(context) {
   const { electronPlatformName, appOutDir } = context;
   if (electronPlatformName !== 'darwin') {
-    return;
+    return
   }
 
-  const appName = context.packager.appInfo.productFilename;
+  const appName = context.packager.appInfo.productFilename
+  const appBundleId = pkg.build.appId
+  if (appBundleId.length < 3) {
+    throw Error(`Invalid build.appId: ${appBundleId}`)
+  }
 
+  console.log(`Notarizing the app "${appBundleId}"`)
   return await notarize({
-    appBundleId: 'org.ooni.probe-desktop',
+    appBundleId: appBundleId,
     appPath: `${appOutDir}/${appName}.app`,
     appleId: process.env.APPLEID,
     appleIdPassword: process.env.APPLEIDPASS,
-  });
-};
+  })
+}
