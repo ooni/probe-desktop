7aa55a317e0f45a30ba99f24f9695b1fc39a7d74
Sarath M S <sarath.ms@gmail.com>
2019-12-02 17:38:34 -0500

    Add component to open links externally

---
+++ renderer/components/ExternalLink.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { Link } from 'ooni-components'
+
+import { openInBrowser } from './utils'
+
+const ExternalLink = ({href, color = 'blue5', children}) => {
+  return (
+    <Link color={color} href={href} onClick={(e) => openInBrowser(href, e)}>
+      {children}
+    </Link>
+  )
+}
+
+ExternalLink.propTypes = {
+  href: PropTypes.string.isRequired,
+  color: PropTypes.string,
+  children: PropTypes.node
+}
+
+export default ExternalLink
--- renderer/components/FormattedMarkdownMessage.js
+++ renderer/components/FormattedMarkdownMessage.js
@@ -1,37 +1,12 @@
-/* global require */
 import React from 'react'
-import PropTypes from 'prop-types'
 import { useIntl } from 'react-intl'
-import styled from 'styled-components'
 import remark from 'remark'
 import reactRenderer from 'remark-react'
 
-import { openInBrowser } from './utils'
-
-const StyledLink = styled.a`
-  color: ${props => props.theme.colors.blue5};
-  text-decoration: none;
-
-  &:hover {
-    color: ${props => props.theme.colors.blue3};
-  }
-`
-
-const OpenLinkInBrowser = ({href, children}) => {
-  return (
-    <StyledLink href={href} onClick={(e) => openInBrowser(href, e)}>
-      {children}
-    </StyledLink>
-  )
-}
-
-OpenLinkInBrowser.propTypes = {
-  href: PropTypes.string.isRequired,
-  children: PropTypes.node
-}
+import ExternalLink from './ExternalLink'
 
 const remarkReactComponents = {
-  a: OpenLinkInBrowser
+  a: ExternalLink
 }
 
 const FormattedMarkdownMessage = ({ id, defaultMessage, values, description }) => {
