feefe922b1e3ea9f741ada4ffa80620352b55a14
Sarath M S <sarath.ms@gmail.com>
2020-03-18 02:37:44 -0400

    Add matomo analytics for screen navigation

--- renderer/pages/_app.js
+++ renderer/pages/_app.js
@@ -1,12 +1,28 @@
 import React from 'react'
 import App from 'next/app'
+import Router from 'next/router'
 import * as Sentry from '@sentry/node'
+import { createInstance } from '@datapunt/matomo-tracker-react'
 
 Sentry.init({
   dsn: 'https://e1eef2aaa6054d94bffc4a648fb78f09@sentry.io/1210892',
 })
 
 class MyApp extends App {
+  componentDidMount() {
+    Router.events.on('routeChangeStart', url => {
+      const instance = createInstance({
+        urlBase: 'https://matomo.ooni.org/',
+        siteId: 3, // optional, default value: `1`
+        trackerUrl: 'https://matomo.ooni.org/matomo.php',
+        srcUrl: 'https://matomo.ooni.org/matomo.js',
+      })
+      instance.trackPageView({
+        href: url
+      })
+    })
+  }
+
   render() {
     const { Component, pageProps } = this.props
 
