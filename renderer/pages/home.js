78acd989ae2d60bbce0af19caa32796b1fb34a2e
Sarath M S <sarath.ms@gmail.com>
2020-03-19 14:44:39 -0400

    Remove early return used for debugging

--- renderer/pages/home.js
+++ renderer/pages/home.js
@@ -222,8 +222,6 @@ class Home extends React.Component {
       this.setState({
         runningTestGroupName: testGroupName
       })
-      // TODO Remove this before merge. Here only to test the animation
-      return
 
       const Runner = remote.require('./utils/ooni/run').Runner
       this.runner = new Runner({testGroupName})
