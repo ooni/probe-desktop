fd03e812f4052e55011a7eb5fff3c734f602b1a5
Sarath M S <sarath.ms@gmail.com>
2020-03-26 12:26:56 -0400

    Reset app data before starting tests

--- test/utils.js
+++ test/utils.js
@@ -5,7 +5,7 @@ const path = require('path')
 const chai = require('chai')
 const chaiAsPromised = require('chai-as-promised')
 
-global.before(function() {
+global.before(async function() {
   chai.should()
   chai.use(chaiAsPromised)
 })
@@ -27,6 +27,24 @@ module.exports = {
   async stopApp(app) {
     if (app && app.isRunning()) {
       await app.stop()
+    }
+  },
+
+  async resetData(app) {
+    console.log('Resetting `$OONI_HOME`...')
+    const resetResponse = await app.client.execute(function() {
+      return require('electron')
+        .remote.require('./actions')
+        .hardReset()
+    })
+    if (resetResponse.value === null) {
+      console.log('...done.')
+    }
+  },
+
+  async screenshotApp(app) {
+    if (app && app.isRunning) {
+      const screenshotBuffer = await app.browserWindow.capturePage()
     }
   }
 }
