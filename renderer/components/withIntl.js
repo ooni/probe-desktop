f466bbfe50694cb5e251d1bbaa0ab3c144af3895
Sarath M S <sarath.ms@gmail.com>
2020-03-31 04:12:04 -0400

    use os-locale to detect system locale

--- renderer/components/withIntl.js
+++ renderer/components/withIntl.js
@@ -1,16 +1,24 @@
 /* global require */
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 import { IntlProvider } from 'react-intl'
+import osLocale from 'os-locale'
 
 let supportedMessages = {
   en: require('../../lang/en.json')
 }
 
 const getLocale = () => {
+  // fallback to en
   let navigatorLang = 'en-US'
+
+  // If available, use locale information provided by window.navigator (chrome)
   if (typeof window !== 'undefined') {
     navigatorLang = window.navigator.userLanguage || window.navigator.language
   }
+
+  // If found, use os-locale to detect user's system locale
+  navigatorLang = osLocale.sync() || navigatorLang
+
   return navigatorLang.split('-')[0]
 }
 
@@ -18,9 +26,9 @@ if (typeof window !== 'undefined' && window.OONITranslations) {
   supportedMessages = window.OONITranslations
 }
 
-const withIntl = (Page) => {
+const withIntl = Page => {
   return class PageWithIntl extends Component {
-    render () {
+    render() {
       const locale = getLocale()
       const now = Date.now()
 
