f0675d5bbfbe703a3e2b6616ff4d37bc3f90ee80
Arturo Filastò <arturo@filasto.net>
2020-04-20 16:46:53 +0200

    Add icon next to ExternalLink
    Fixes one of the points in ooni/probe#1098

--- renderer/components/ExternalLink.js
+++ renderer/components/ExternalLink.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+
+import { MdOpenInNew } from 'react-icons/md'
+
 import { Link } from 'ooni-components'
 
 import { openInBrowser } from './utils'
 
 const ExternalLink = ({href, color = 'blue5', children, ...props}) => (
   <Link {...props} color={color} href={href} onClick={(e) => openInBrowser(href  , e)}>
-    {children}
+      {children}
+      <MdOpenInNew />
   </Link>
 )
 
