23010b265da9be9e55642effa4047523a58339fa
Sarath M S <sarath.ms@gmail.com>
2021-02-12 16:44:43 -0500

    Add a way to skip notarization when building for macos

--- scripts/notarize.js
+++ scripts/notarize.js
@@ -3,8 +3,8 @@ const pkg = require('../package.json')
 const { notarize } = require('electron-notarize')
 
 exports.default = async function notarizing(context) {
-  const { electronPlatformName, appOutDir } = context;
-  if (electronPlatformName !== 'darwin') {
+  const { electronPlatformName, appOutDir } = context
+  if (electronPlatformName !== 'darwin' || process.env.CSC_IDENTITY_AUTO_DISCOVERY === 'false') {
     return
   }
 
