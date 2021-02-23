22be8db6f3b39adf2bdf380247a2dcb08f8d6af7
Sarath M S <sarath.ms@gmail.com>
2021-02-23 17:18:21 -0500

    Convert classes to functional components

--- renderer/pages/result.js
+++ renderer/pages/result.js
@@ -1,9 +1,9 @@
 /* global require */
+import React, { useState, useEffect } from 'react'
 import electron from 'electron'
-import React from 'react'
-import Raven from 'raven-js'
-
-import { withRouter } from 'next/router'
+import * as Sentry from '@sentry/node'
+import { useRouter } from 'next/router'
+import Debug from 'debug'
 
 import Layout from '../components/Layout'
 import Sidebar from '../components/Sidebar'
@@ -11,7 +11,7 @@ import ResultContainer from '../components/result/ResultContainer'
 import ErrorView from '../components/ErrorView'
 import LoadingOverlay from '../components/LoadingOverlay'
 
-const debug = require('debug')('ooniprobe-desktop.renderer.pages.result')
+const debug = Debug('ooniprobe-desktop.renderer.pages.result')
 
 const sortMeasurementRows = (rows) => {
   return rows.sort((a, b) => {
@@ -25,22 +25,14 @@ const sortMeasurementRows = (rows) => {
   })
 }
 
-class Result extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      loading: true,
-      resultsList: {},
-      measurementRows: [],
-      measurementSummary: {},
-      selectedMeasurement: {},
-      error: null
-    }
-    this.loadMeasurements = this.loadMeasurements.bind(this)
-    this.loadData = this.loadData.bind(this)
-  }
+const Result = () => {
+  const { query } = useRouter()
+  const [loading, setLoading] = useState(true)
+  const [error, setError] = useState(null)
+  const [measurementRows, setMeasurementRows] = useState([])
+  const [measurementSummary, setMeasurementSummary] = useState({})
 
-  loadMeasurements(resultID) {
+  const loadMeasurements = (resultID) => {
     debug('listing result_id ', resultID)
     const remote = electron.remote
     const { listMeasurements } = remote.require('./actions')
@@ -49,61 +41,40 @@ class Result extends React.Component {
         rows,
         summary
       } = measurementList
-      return this.setState({
-        loading: false,
-        measurementSummary: summary,
-        measurementRows: sortMeasurementRows(rows)
-      })
+
+      setLoading(false)
+      setError(null)
+      setMeasurementSummary(summary)
+      setMeasurementRows(sortMeasurementRows(rows))
+
     }).catch(err => {
-      Raven.captureException(err, {extra: {scope: 'renderer.listMeasurements'}})
-      debug('error triggered', err)
-      return this.setState({
-        error: err
+      Sentry.withScope((scope) => {
+        scope.setTag('context', 'renderer.listMeasurements')
+        Sentry.captureException(err)
       })
+      debug('error triggered', err)
+      setError(err)
     })
   }
 
-  loadData() {
-    const { query } = this.props.router
+  useEffect(() =>{
     debug('load data with', query)
-    return this.loadMeasurements(query.resultID)
-  }
+    return loadMeasurements(query.resultID)
+  }, [query])
 
-  componentDidMount() {
-    this.loadData()
-  }
+  const { resultID } = query
+  debug('loading', resultID)
 
-  componentDidUpdate(prevProps) {
-    if (this.props.router.query !== prevProps.router.query) {
-      this.loadData()
-    }
-  }
+  return (
+    <Layout>
+      <Sidebar>
+        <LoadingOverlay loading={loading} />
+        {!loading && <ResultContainer rows={measurementRows} summary={measurementSummary} />}
+        {error && <ErrorView error={error} />}
+      </Sidebar>
+    </Layout>
+  )
 
-  render() {
-    const {
-      loading,
-      measurementRows,
-      measurementSummary,
-      error
-    } = this.state
-
-    const {
-      resultID
-    } = this.props.router.query
-
-    debug('loading', resultID)
-
-    return (
-      <Layout>
-        <Sidebar>
-          <LoadingOverlay loading={loading} />
-          {!loading && <ResultContainer rows={measurementRows} summary={measurementSummary} />}
-          {error && <ErrorView error={error} />}
-        </Sidebar>
-      </Layout>
-    )
-
-  }
 }
 
-export default withRouter(Result)
+export default Result
--- renderer/pages/test-results.js
+++ renderer/pages/test-results.js
@@ -1,9 +1,7 @@
 /* global require */
 import electron from 'electron'
-import React from 'react'
-import Raven from 'raven-js'
-
-import { withRouter } from 'next/router'
+import React, { useState, useEffect } from 'react'
+import * as Sentry from '@sentry/node'
 
 import Layout from '../components/Layout'
 import Sidebar from '../components/Sidebar'
@@ -13,62 +11,35 @@ import LoadingOverlay from '../components/LoadingOverlay'
 
 const debug = require('debug')('ooniprobe-desktop.renderer.pages.results')
 
-class TestResults extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      loading: true,
-      results: {},
-      error: null
-    }
-    this.loadResults = this.loadResults.bind(this)
-  }
+const TestResults = () => {
+  const [loading, setLoading] = useState(true)
+  const [results, setResults] = useState({})
+  const [error, setError] = useState(null)
 
-  loadResults() {
+  useEffect(() => {
     const remote = electron.remote
     const { listResults } = remote.require('./actions')
 
-    return listResults().then(results => {
-      return this.setState({
-        loading: false,
-        results,
-      })
+    listResults().then(listedResults => {
+      setResults(listedResults)
+      setLoading(false)
     }).catch(err => {
-      Raven.captureException(err, {extra: {scope: 'renderer.listResults'}})
+      Sentry.captureException(err, {extra: {scope: 'renderer.listResults'}})
       debug('error triggered', err)
-      return this.setState({
-        error: err
-      })
+      setError(err)
+      setLoading(false)
     })
-  }
+  }, [])
 
-  componentDidMount() {
-    this.loadResults()
-  }
-
-  componentDidUpdate(prevProps) {
-    if (this.props.router.query !== prevProps.router.query) {
-      this.loadResults()
-    }
-  }
-
-  render() {
-    const {
-      loading,
-      results,
-      error
-    } = this.state
-
-    return (
-      <Layout>
-        <Sidebar>
-          <LoadingOverlay loading={loading} />
-          {!loading && <TestResultsContainer results={results} />}
-          {error && <ErrorView error={error} />}
-        </Sidebar>
-      </Layout>
-    )
-  }
+  return (
+    <Layout>
+      <Sidebar>
+        <LoadingOverlay loading={loading} />
+        {!loading && <TestResultsContainer results={results} />}
+        {error && <ErrorView error={error} />}
+      </Sidebar>
+    </Layout>
+  )
 }
 
-export default withRouter(TestResults)
+export default TestResults
