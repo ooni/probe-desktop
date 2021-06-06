ee56ab55bc8c51cdc8ee747bf7a76679d9662bdf
Eddges <shivamshekhar.i7@gmail.com>
2021-06-06 18:58:06 +0530

    jest configurations moved from package.json to jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,9 @@
 module.exports = {
     testPathIgnorePatterns: ["/.next/", "/node_modules/"],
-    setupFilesAfterEnv: ["./setupTests.js"]
+    setupFilesAfterEnv: ["./setupTests.js"],
+    verbose: true,
+    roots: ["<rootDir>/test"],
+    testMatch: ["**/test/**/*.e2e.js", "**/test/**/*.test.js"],
+    setupFiles: ["<rootDir>/test/lib/setup.js"],
+    testSequencer: "<rootDir>/test/lib/sequencer.js",
 };
