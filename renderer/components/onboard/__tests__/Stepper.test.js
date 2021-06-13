c7503396992579b69529191de3cd4e74243d7cb9
Eddges <shivamshekhar.i7@gmail.com>
2021-06-13 15:19:43 +0530

    Snapshot test fixed and snapshots updated for Stepper.test.js

--- renderer/components/onboard/__tests__/Stepper.test.js
+++ renderer/components/onboard/__tests__/Stepper.test.js
@@ -2,26 +2,12 @@
  * @jest-environment jsdom
  */
 
-import { render, cleanup } from "@testing-library/react";
+import { cleanup } from "@testing-library/react";
 import { theme } from "ooni-components";
 import { ThemeProvider } from "styled-components";
-import { IntlProvider } from "react-intl";
 import renderer from "react-test-renderer";
-import English from "../../../../lang/en.json";
 
 import Stepper from "../Stepper";
-
-const HydratedComponent = (
-  { mainComponent },
-  locale = "en",
-  messages = English
-) => {
-  return (
-    <IntlProvider locale={locale} messages={messages}>
-      <ThemeProvider theme={theme}>{mainComponent}</ThemeProvider>
-    </IntlProvider>
-  );
-};
 
 describe("Tests from Stepper component", () => {
   afterEach(() => {
@@ -29,19 +15,31 @@ describe("Tests from Stepper component", () => {
   });
   test("With active index 0", async () => {
     const component = renderer
-      .create(<HydratedComponent mainComponent={`${<Stepper activeIdx={0} />}`} />)
+      .create(
+        <ThemeProvider theme={theme}>
+          <Stepper activeIdx={0} />
+        </ThemeProvider>
+      )
       .toJSON();
     expect(component).toMatchSnapshot();
   });
   test("With active index 1", async () => {
     const component = renderer
-      .create(<HydratedComponent mainComponent={`${<Stepper activeIdx={1} />}`} />)
+      .create(
+        <ThemeProvider theme={theme}>
+          <Stepper activeIdx={1} />
+        </ThemeProvider>
+      )
       .toJSON();
     expect(component).toMatchSnapshot();
   });
   test("With active index 2", async () => {
     const component = renderer
-      .create(<HydratedComponent mainComponent={`${<Stepper activeIdx={2} />}`} />)
+      .create(
+        <ThemeProvider theme={theme}>
+          <Stepper activeIdx={2} />
+        </ThemeProvider>
+      )
       .toJSON();
     expect(component).toMatchSnapshot();
   });
