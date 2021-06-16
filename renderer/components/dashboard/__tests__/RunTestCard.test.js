17a27628612d26fa756b2cd0f3856cf7f0a54e7a
Eddges <shivamshekhar.i7@gmail.com>
2021-06-17 00:38:39 +0530

    Tests for RunTestCard written

--- renderer/components/dashboard/__tests__/RunTestCard.test.js
+++ renderer/components/dashboard/__tests__/RunTestCard.test.js
@@ -2,7 +2,7 @@
  * @jest-environment jsdom
  */
 
-import { screen, render, cleanup } from "@testing-library/react";
+import { screen, render, cleanup, fireEvent } from "@testing-library/react";
 import { theme } from "ooni-components";
 import { ThemeProvider } from "styled-components";
 import { IntlProvider } from "react-intl";
@@ -34,7 +34,7 @@ describe("Tests for RunTestCard component", () => {
   afterEach(() => {
     cleanup();
   });
-  test("If all the Test Cards are mounted", async () => {
+  test("All the Test Cards are mounted", async () => {
     const router = useRouter();
     testList.map((t, idx) => {
       renderComponent(
@@ -50,5 +50,19 @@ describe("Tests for RunTestCard component", () => {
       expect(screen.getByText(English[t.description.props.id])).toBeInTheDocument()
     }
     );
+  });
+  test("Individual Test Cards work as expected", async () => {
+    const loadTest = jest.fn();
+    const websiteDetails = testList[0];
+    renderComponent(
+      <RunTestCard
+        onClick={() => loadTest()}
+        id={websiteDetails.key}
+        {...websiteDetails}
+      />
+    );
+    const testCard = screen.getByTestId('card')
+    fireEvent.click(testCard)
+    expect(loadTest).toHaveBeenCalledTimes(1)
   });
 });
