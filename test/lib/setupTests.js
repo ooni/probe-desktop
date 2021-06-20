93848fdd5dbba973de3d6b8bd1859e9eb5b54535
Eddges <shivamshekhar.i7@gmail.com>
2021-06-20 16:56:52 +0530

    Writing tests for LanguageSelector

--- test/lib/setupTests.js
+++ test/lib/setupTests.js
@@ -1,3 +1,4 @@
-import "jest-canvas-mock";
-import "jest-styled-components";
-import "@testing-library/jest-dom/extend-expect";
+import 'jest-canvas-mock'
+import 'jest-styled-components'
+import '@testing-library/jest-dom/extend-expect'
+jest.setTimeout(30000)
