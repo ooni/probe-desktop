b8e6064011ca820815f9f914b10ffd873a293e71
Eddges <shivamshekhar.i7@gmail.com>
2021-06-28 15:36:19 +0530

    Tests written for UrlList.js

--- renderer/components/dashboard/__tests__/UrlList.test.js
+++ renderer/components/dashboard/__tests__/UrlList.test.js
@@ -8,6 +8,7 @@ import { ThemeProvider } from 'styled-components'
 import { IntlProvider } from 'react-intl'
 import { screen, render, fireEvent, cleanup } from '@testing-library/react'
 import English from '../../../../lang/en.json'
+import { ipcRenderer } from 'electron'
 
 import UrlList from '../UrlList'
 
@@ -30,23 +31,52 @@ const renderComponent = (component, locale = 'en', messages = English) => {
 }
 
 describe('Tests for UrlList.js', () => {
+
+  const websiteList = ['https://www.twitter.com', 'https://www.facebook.com']
+
   beforeEach(() => {
     renderComponent(<UrlList incomingList={null} />)
   })
   afterEach(() => {
     cleanup()
   })
+
   test('Run button is disabled by default', async () => {
     const runButton = screen.getByRole('button', {
       name: English['Settings.Websites.CustomURL.Run'],
     })
     expect(runButton).toBeDisabled()
   })
+
+  test('Run button is disabled on entering incorrect URL', async () => {
+    const urlInputBox = screen.getByRole('textbox')
+    expect(urlInputBox).toBeInTheDocument()
+    fireEvent.change(urlInputBox, {
+      target: { value: 'https://www.twitter.c' },
+    })
+    const runButton = screen.getByRole('button', {
+      name: English['Settings.Websites.CustomURL.Run'],
+    })
+    expect(runButton).toBeDisabled()
+  })
+
+  test('Run button is enabled on entering correct URL', async () => {
+    const urlInputBox = screen.getByRole('textbox')
+    expect(urlInputBox).toBeInTheDocument()
+    fireEvent.change(urlInputBox, {
+      target: { value: websiteList[0] },
+    })
+    const runButton = screen.getByRole('button', {
+      name: English['Settings.Websites.CustomURL.Run'],
+    })
+    expect(runButton).not.toBeDisabled()
+  })
+
   test('URL is correctly added and a new textbox is created', async () => {
     const urlInputBox = screen.getByRole('textbox')
     expect(urlInputBox).toBeInTheDocument()
     fireEvent.change(urlInputBox, {
-      target: { value: 'https://www.twitter.com' },
+      target: { value: websiteList[0] },
     })
     const addURLButton = screen.getByRole('button', {
       name: English['Settings.Websites.CustomURL.Add'],
@@ -55,17 +85,18 @@ describe('Tests for UrlList.js', () => {
     fireEvent.click(addURLButton)
     const urlInputBoxes = screen.getAllByRole('textbox')
     expect(urlInputBoxes).toHaveLength(2)
-    expect(urlInputBoxes[0].value).toBe('https://www.twitter.com')
+    expect(urlInputBoxes[0].value).toBe(websiteList[0])
     expect(urlInputBoxes[1].value).toBe('')
     const runButton = screen.getByRole('button', {
       name: English['Settings.Websites.CustomURL.Run'],
     })
     expect(runButton).toBeDisabled()
   })
+
   test('Added URL can be removed', async () => {
     const urlInputBox = screen.getByRole('textbox')
     fireEvent.change(urlInputBox, {
-      target: { value: 'https://www.twitter.com' },
+      target: { value: websiteList[0] },
     })
     const addURLButton = screen.getByRole('button', {
       name: English['Settings.Websites.CustomURL.Add'],
@@ -74,7 +105,7 @@ describe('Tests for UrlList.js', () => {
     fireEvent.click(addURLButton)
     const newUrlInputBox = screen.getAllByRole('textbox')[1]
     fireEvent.change(newUrlInputBox, {
-      target: { value: 'https://www.facebook.com' },
+      target: { value: websiteList[1] },
     })
     const removeFirstURL = screen.getByTestId('urlRemove0')
     const removeSecondURL = screen.getByTestId('urlRemove1')
@@ -84,38 +115,26 @@ describe('Tests for UrlList.js', () => {
     expect(urlInputBoxes).toHaveLength(1)
   })
 
-  // There is some buggy behavior with Run button as it doesn't enable on entering correct URLs
-
   test('Custom websites test is triggered on entering a valid URL', async () => {
     const urlInputBox = screen.getByRole('textbox')
     fireEvent.change(urlInputBox, {
-      target: { value: 'https://www.twitter.com' },
+      target: { value: websiteList[0] },
     })
-    fireEvent.keyDown(urlInputBox, {
-      key: 'Tab',
-      code: 9,
-      charCode: 9
+    const addURLButton = screen.getByRole('button', {
+      name: English['Settings.Websites.CustomURL.Add'],
+      bubbles: true,
     })
-    // const addURLButton = screen.getByRole('button', {
-    //   name: English['Settings.Websites.CustomURL.Add'],
-    //   bubbles: true,
-    // })
-    // fireEvent.click(addURLButton)
-    // const newUrlInputBox = screen.getAllByRole('textbox')[1]
-    // console.log('prev line')
-    // fireEvent.change(newUrlInputBox, {
-    //   target: { value: 'https://www.facebook.com' },
-    // })
-    // fireEvent.click(addURLButton)
-
-    // const removeThirdUrlInput = screen.getByTestId('urlRemove2')
-    // fireEvent.click(removeThirdUrlInput)
+    fireEvent.click(addURLButton)
+    const newUrlInputBox = screen.getAllByRole('textbox')[1]
+    fireEvent.change(newUrlInputBox, {
+      target: { value: websiteList[1] },
+    })
     const runButton = screen.getByRole('button', {
       name: English['Settings.Websites.CustomURL.Run'],
     })
     expect(runButton).not.toBeDisabled()
     fireEvent.click(runButton)
-    expect(mockRouterPush).toHaveBeenCalledWith('/dashboard/running')
-    expect(2).toBe(2)
+    expect(ipcRenderer.send).toHaveBeenCalledTimes(1)
+    expect(ipcRenderer.send).toHaveBeenCalledWith('fs.write.request', websiteList.join('\n'))
   })
 })
