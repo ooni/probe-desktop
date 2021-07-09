fe788c60509cabb58e88b45cd0c86fb8b3b392e5
Eddges <shivamshekhar.i7@gmail.com>
2021-07-10 01:23:18 +0530

    Chilprocess spawn test written

--- main/utils/ooni/__tests__/ooniprobe.test.js
+++ main/utils/ooni/__tests__/ooniprobe.test.js
@@ -1,4 +1,5 @@
 import { Ooniprobe } from '../ooniprobe'
+import childProcess from 'child_process'
 import log from 'electron-log'
 
 jest.mock('electron-util', () => ({
@@ -11,6 +12,30 @@ jest.mock('electron-util', () => ({
 }))
 
 log.debug = jest.fn()
+log.info = jest.fn()
+log.verbose = jest.fn()
+
+jest.mock('child_process', () => ({
+  spawn: jest.fn(() => ({
+    stdin: {
+      end: jest.fn()
+    },
+    on: jest.fn(),
+    stderr: {
+      on: jest.fn()
+    },
+    stdout: {
+      pipe: jest.fn(() => ({
+        on: jest.fn()
+      }))
+    }
+  }))
+}))
+
+jest.mock('../../paths', () => ({
+  getHomeDir: jest.fn(() => '/home/user/probe-desktop/ooni_home'),
+  getBinaryPath: jest.fn(() => 'build/probe-cli/linux_amd64')
+}))
 
 describe('Test the initialization of Ooniprobe instances', () => {
   test('Ooniprobe instances are initialized in an expected way', async () => {
@@ -27,7 +52,7 @@ describe('Test the initialization of Ooniprobe instances', () => {
 })
 
 describe('Tests if killing Ooniprobe instances work as expected', () => {
-  test('Killing Ooniprobe instance with .ooni property calls this.ooni.stdin.end function', async () => {
+  test('Killing Ooniprobe instance with truthy .ooni property calls stdin.end function', async () => {
     const ooniInstance = new Ooniprobe()
     ooniInstance.ooni = {
       stdin: {
@@ -38,8 +63,9 @@ describe('Tests if killing Ooniprobe instances work as expected', () => {
     expect(ooniInstance.ooni.stdin.end).toHaveBeenCalledTimes(1)
   })
 
-  test('Killing Ooniprobe instance with .ooni as null throws error', async () => {
+  test('Killing Ooniprobe instance if .ooni is null throws error', async () => {
     const ooniInstance = new Ooniprobe()
+    const errorMessage = 'cannot kill an unstarted process'
     const errorFunc = () => {
       try {
         ooniInstance.kill()
@@ -47,6 +73,31 @@ describe('Tests if killing Ooniprobe instances work as expected', () => {
         throw Error(err)
       }
     }
-    expect(errorFunc).toThrow('cannot kill an unstarted process')
+    expect(errorFunc).toThrow(errorMessage)
+  })
+})
+
+describe('Ooniprobe instances invokes .call() method', () => {
+  test('ChildProcess is spawned on .call()', async () => {
+    const binaryPath = 'build/probe-cli/linux_amd64'
+    const args = [
+      '--batch',
+      '--software-name=ooniprobe-desktop',
+      '--software-version=3.6.0-dev',
+      'list'
+    ]
+    const options = {
+      stdio: ['pipe', 'pipe', 'pipe'],
+      env: {
+        'OONI_HOME': '/home/user/probe-desktop/ooni_home',
+        'OONI_STDIN_EOF_IMPLIES_SIGTERM': 'true'
+      }
+    }
+    const ooniInstance = new Ooniprobe()
+    ooniInstance.call(['list'])
+    expect(childProcess.spawn).toHaveBeenCalledTimes(1)
+    expect(childProcess.spawn.mock.calls[0][0]).toBe(binaryPath)
+    expect(childProcess.spawn.mock.calls[0][1]).toMatchObject(args)
+    expect(childProcess.spawn.mock.calls[0][2]).toMatchObject(options)
   })
 })
