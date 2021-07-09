991b6f674ce3f70952e839142f7a9778482b9137
Eddges <shivamshekhar.i7@gmail.com>
2021-07-09 23:57:55 +0530

    Tests to check proper initialization of Ooniprobe instance

--- main/utils/ooni/__tests__/ooniprobe.test.js
+++ main/utils/ooni/__tests__/ooniprobe.test.js
@@ -12,6 +12,20 @@ jest.mock('electron-util', () => ({
 
 log.debug = jest.fn()
 
+describe('Test the initialization of Ooniprobe instances', () => {
+  test('Ooniprobe instances are initialized in an expected way', async () => {
+    const ooniInstance = new Ooniprobe()
+    expect(ooniInstance._binaryPath).toBeTruthy()
+    expect(ooniInstance._binaryPath).toMatch(/amd64/i)
+    expect(ooniInstance.ooni).toBeNull()
+
+    // Checking if the kill and call functions are
+    // defined in the initialized Ooniprobe instance
+    expect(ooniInstance.kill).toBeTruthy()
+    expect(ooniInstance.call).toBeTruthy()
+  })
+})
+
 describe('Tests if killing Ooniprobe instances work as expected', () => {
   test('Killing Ooniprobe instance with .ooni property calls this.ooni.stdin.end function', async () => {
     const ooniInstance = new Ooniprobe()
