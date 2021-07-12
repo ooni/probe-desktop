d4f4f4a1e1bd35c4a40c6e38facbe039e1d48919
Eddges <shivamshekhar.i7@gmail.com>
2021-07-12 18:31:38 +0530

    Mocking childProcess.spawn individually for tests

--- main/utils/ooni/__tests__/ooniprobe.test.js
+++ main/utils/ooni/__tests__/ooniprobe.test.js
@@ -14,27 +14,15 @@ jest.mock('electron-util', () => ({
 log.debug = jest.fn()
 log.info = jest.fn()
 log.verbose = jest.fn()
+log.error = jest.fn()
 
 jest.mock('child_process', () => ({
-  spawn: jest.fn(() => ({
-    stdin: {
-      end: jest.fn()
-    },
-    on: jest.fn(),
-    stderr: {
-      on: jest.fn()
-    },
-    stdout: {
-      pipe: jest.fn(() => ({
-        on: jest.fn()
-      }))
-    }
-  }))
+  spawn: jest.fn()
 }))
 
 jest.mock('../../paths', () => ({
   getHomeDir: jest.fn(() => '/home/user/probe-desktop/ooni_home'),
-  getBinaryPath: jest.fn(() => 'build/probe-cli/linux_amd64')
+  getBinaryPath: jest.fn(() => 'build/probe-cli/linux_amd64'),
 }))
 
 describe('Test the initialization of Ooniprobe instances', () => {
@@ -56,8 +44,8 @@ describe('Tests if killing Ooniprobe instances work as expected', () => {
     const ooniInstance = new Ooniprobe()
     ooniInstance.ooni = {
       stdin: {
-        end: jest.fn()
-      }
+        end: jest.fn(),
+      },
     }
     ooniInstance.kill()
     expect(ooniInstance.ooni.stdin.end).toHaveBeenCalledTimes(1)
@@ -69,7 +57,7 @@ describe('Tests if killing Ooniprobe instances work as expected', () => {
     const errorFunc = () => {
       try {
         ooniInstance.kill()
-      } catch(err) {
+      } catch (err) {
         throw Error(err)
       }
     }
@@ -78,27 +66,75 @@ describe('Tests if killing Ooniprobe instances work as expected', () => {
 })
 
 describe('Ooniprobe instances invokes .call() method', () => {
+  beforeAll(() => {
+    childProcess.spawn.mockImplementation(() => ({
+      stdin: {
+        end: jest.fn(),
+      },
+      on: jest.fn(),
+      stderr: {
+        on: jest.fn(),
+      },
+      stdout: {
+        pipe: jest.fn(() => ({
+          on: jest.fn(),
+        })),
+      },
+    }))
+  })
+
   test('ChildProcess is spawned on .call()', async () => {
     const binaryPath = 'build/probe-cli/linux_amd64'
     const args = [
       '--batch',
       '--software-name=ooniprobe-desktop',
-      '--software-version=3.5.2', // For CI
-      // '--software-version=3.6.0-dev', // For dev environment
-      'list'
+      // '--software-version=3.5.2', // For CI
+      '--software-version=3.6.0-dev', // For dev environment
+      'list',
     ]
     const options = {
       stdio: ['pipe', 'pipe', 'pipe'],
       env: {
-        'OONI_HOME': '/home/user/probe-desktop/ooni_home',
-        'OONI_STDIN_EOF_IMPLIES_SIGTERM': 'true'
-      }
+        OONI_HOME: '/home/user/probe-desktop/ooni_home',
+        OONI_STDIN_EOF_IMPLIES_SIGTERM: 'true',
+      },
     }
     const ooniInstance = new Ooniprobe()
     ooniInstance.call(['list'])
     expect(childProcess.spawn).toHaveBeenCalledTimes(1)
     expect(childProcess.spawn.mock.calls[0][0]).toBe(binaryPath)
-    expect(childProcess.spawn.mock.calls[0][1].toString()).toMatch(args.toString())
+    expect(childProcess.spawn.mock.calls[0][1].toString()).toMatch(
+      args.toString()
+    )
     expect(childProcess.spawn.mock.calls[0][2]).toMatchObject(options)
+  })
+})
+
+describe('Ooniprobe instances can call the IPC events', () => {
+  beforeEach(() => {
+    jest.clearAllMocks()
+  })
+  test('ChildProcess is spawned on .call()', async () => {
+    childProcess.spawn.mockImplementation(() => ({
+      stdin: {
+        end: jest.fn(),
+      },
+      on: jest.fn((event, callback) => {
+        console.log('pepe: ', event)
+        callback()
+      }),
+      stderr: {
+        on: jest.fn(),
+      },
+      stdout: {
+        pipe: jest.fn(() => ({
+          on: jest.fn(),
+        })),
+      },
+    }))
+    const ooniInstance = new Ooniprobe()
+    const calledPromise = ooniInstance.call(['list'])
+    console.log('calls: ', childProcess.spawn.mock.calls)
+    expect(2).toBeTruthy()
   })
 })
