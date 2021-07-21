60a2d98990232d0125c788520b90637c0efe38c6
Eddges <shivamshekhar.i7@gmail.com>
2021-07-21 17:58:02 +0530

    test to assert that en.json contains all the ids

--- scripts/__tests__/translations.test.js
+++ scripts/__tests__/translations.test.js
@@ -7,6 +7,8 @@ describe('Tests for translations', () => {
 
   const langIdArray = csvParse(readFileSync('./data/lang-en.csv'), {from: 2}).map(row => row[0])
 
+  const supportedLanguages = glob.sync('./lang/*.json').map((f) => basename(f, '.json'))
+
   test('Any supported languages are not missing', async () => {
     const expectedLanguagesArray = [
       'ar', 'ca', 'de',
@@ -18,8 +20,13 @@ describe('Tests for translations', () => {
       'tr', 'zh-rCN', 'zh-rTW',
     ]
 
-    const supportedLanguages = glob.sync('./lang/*.json').map((f) => basename(f, '.json'))
-
     expect(supportedLanguages.sort()).toEqual(expectedLanguagesArray.sort())
+  })
+
+  test('en.json contains all the ids', async () => {
+    const En = JSON.parse(readFileSync('./lang/en.json'))
+
+    const missingMessageArray = langIdArray.filter(id => En[id] ? false : true)
+    expect(missingMessageArray).toHaveLength(0)
   })
 })
