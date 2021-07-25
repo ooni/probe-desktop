9b6347c830e5adfbe304772161c40ff276927902
Eddges <shivamshekhar.i7@gmail.com>
2021-07-25 20:46:18 +0530

    Pauses added in tests for better assertions

--- test/runtest.e2e.js
+++ test/runtest.e2e.js
@@ -3,100 +3,100 @@ import { waitFor } from '@testing-library/dom'
 
 jest.setTimeout(600000)
 
-// describe('IM test', () => {
-//   let app
+describe('IM test', () => {
+  let app
 
-//   beforeAll(async () => {
-//     app = await startApp()
-//   })
+  beforeAll(async () => {
+    app = await startApp()
+  })
 
-//   afterAll(async () => {
-//     await stopApp(app)
-//   })
+  afterAll(async () => {
+    await stopApp(app)
+  })
 
-//   test('IM test successfully starts', async () => {
-//     await app.client.$('div[data-testid=run-card-im]').click().pause(5000)
+  test('IM test successfully starts', async () => {
+    await app.client.$('div[data-testid=run-card-im]').click().pause(1500)
 
-//     await app.client.$('button[data-testid=button-run-test]').click()
+    await app.client.$('button[data-testid=button-run-test]').click()
 
-//     const preparingTestsVisible = await app.client.isVisible(
-//       'span=Preparing test...'
-//     )
-//     expect(preparingTestsVisible).toBe(true)
-//   })
+    const preparingTestsVisible = await app.client.isVisible(
+      'span=Preparing test...'
+    )
+    expect(preparingTestsVisible).toBe(true)
+  })
 
-//   test('IM test runs with all 4 network tests', async () => {
-//     await waitFor(
-//       async () => {
-//         const messengerTestName = await app.client.getText(
-//           'div[data-testid=text-running-test-name]'
-//         )
-//         return expect(messengerTestName).toBe('Facebook Messenger Test')
-//       },
-//       { timeout: 120000 }
-//     )
+  test('IM test runs with all 4 network tests', async () => {
+    await waitFor(
+      async () => {
+        const messengerTestName = await app.client.getText(
+          'div[data-testid=text-running-test-name]'
+        )
+        return expect(messengerTestName).toBe('Facebook Messenger Test')
+      },
+      { timeout: 120000 }
+    )
 
-//     await waitFor(
-//       async () => {
-//         const telegramTestName = await app.client.getText(
-//           'div[data-testid=text-running-test-name]'
-//         )
-//         return expect(telegramTestName).toBe('Telegram Test')
-//       },
-//       { timeout: 120000 }
-//     )
+    await waitFor(
+      async () => {
+        const telegramTestName = await app.client.getText(
+          'div[data-testid=text-running-test-name]'
+        )
+        return expect(telegramTestName).toBe('Telegram Test')
+      },
+      { timeout: 120000 }
+    )
 
-//     await waitFor(
-//       async () => {
-//         const whatsAppTestHeading = await app.client.getText(
-//           'div[data-testid=text-running-test-name]'
-//         )
-//         return expect(whatsAppTestHeading).toBe('WhatsApp Test')
-//       },
-//       { timeout: 120000 }
-//     )
+    await waitFor(
+      async () => {
+        const whatsAppTestHeading = await app.client.getText(
+          'div[data-testid=text-running-test-name]'
+        )
+        return expect(whatsAppTestHeading).toBe('WhatsApp Test')
+      },
+      { timeout: 120000 }
+    )
 
-//     await waitFor(
-//       async () => {
-//         const signalTestHeading = await app.client.getText(
-//           'div[data-testid=text-running-test-name]'
-//         )
-//         return expect(signalTestHeading).toBe('Signal Test')
-//       },
-//       { timeout: 120000 }
-//     )
+    await waitFor(
+      async () => {
+        const signalTestHeading = await app.client.getText(
+          'div[data-testid=text-running-test-name]'
+        )
+        return expect(signalTestHeading).toBe('Signal Test')
+      },
+      { timeout: 120000 }
+    )
 
-//     await waitFor(
-//       async () => {
-//         const animationVisible = await app.client.isVisible(
-//           'div[data-testid=running-animation-im]'
-//         )
-//         expect(animationVisible).toBe(false)
-//       },
-//       { timeout: 120000 }
-//     )
+    await waitFor(
+      async () => {
+        const animationVisible = await app.client.isVisible(
+          'div[data-testid=running-animation-im]'
+        )
+        expect(animationVisible).toBe(false)
+      },
+      { timeout: 120000 }
+    )
 
-//     await app.client.pause(1000)
-//   })
+    await app.client.pause(1000)
+  })
 
-//   test('Test result data is stored in an expected fashion', async () => {
-//     await app.client
-//       .$('div[data-testid=test-result-im]')
-//       .click()
-//       .pause(2000)
+  test('Test result data is stored in an expected fashion', async () => {
+    await app.client
+      .$('div[data-testid=test-result-im]')
+      .click()
+      .pause(2000)
 
-//     const rowResultLength = await app.client.$$(
-//       'div[data-testid=measured-test-name]'
-//     )
+    const rowResultLength = await app.client.$$(
+      'div[data-testid=measured-test-name]'
+    )
 
-//     expect(rowResultLength).toHaveLength(4)
+    expect(rowResultLength).toHaveLength(4)
 
-//     await app.client
-//       .$('div[data-testid=measured-test-name]')
-//       .click()
-//       .pause(2000)
-//   })
-// })
+    await app.client
+      .$('div[data-testid=measured-test-name]')
+      .click()
+      .pause(2000)
+  })
+})
 
 describe('Websites test', () => {
   let app
@@ -110,9 +110,9 @@ describe('Websites test', () => {
   })
 
   test('Website measurement test loads correctly', async () => {
-    await app.client.$('div[data-testid=run-card-websites]').click()
+    await app.client.$('div[data-testid=run-card-websites]').click().pause(1500)
 
-    await app.client.$('button[data-testid=button-run-test]').click()
+    await app.client.$('button[data-testid=button-run-test]').click().pause(1500)
 
     const headingTestGroupName = await app.client.getText(
       'h2[data-testid=heading-test-group-name]'
@@ -199,9 +199,9 @@ describe('Custom websites test', () => {
   })
 
   test('Run button is disabled by default', async () => {
-    await app.client.$('div[data-testid=run-card-websites]').click()
+    await app.client.$('div[data-testid=run-card-websites]').click().pause(1500)
 
-    await app.client.$('button[data-testid=button-choose-websites]').click()
+    await app.client.$('button[data-testid=button-choose-websites]').click().pause(1500)
 
     const runButtonEnabled = await app.client.isEnabled(
       'button[data-testid=button-run-custom-test]'
