a81ee8b83e3319a9a79d0659d58814888cdfd5d3
Eddges <shivamshekhar.i7@gmail.com>
2021-07-25 17:00:02 +0530

    1. Added some data-testid and modified in places of existing ones in test files. 2. Wrote some assertions for custom URL test for websites

--- renderer/components/dashboard/AddUrlButton.js
+++ renderer/components/dashboard/AddUrlButton.js
@@ -4,7 +4,7 @@ import { FormattedMessage } from 'react-intl'
 import { MdAdd } from 'react-icons/md'
 
 const AddUrlButton = ({ onClick }) => (
-  <Button hollow onClick={onClick}>
+  <Button hollow onClick={onClick} data-testid='button-add-url'>
     <Flex alignItems='center'>
       <MdAdd size={24} /><FormattedMessage id='Settings.Websites.CustomURL.Add' />
     </Flex>
