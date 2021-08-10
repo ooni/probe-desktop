0d016b02bcd5c025062d38e17576df2bbe6d7b70
Eddges <shivamshekhar.i7@gmail.com>
2021-08-10 18:34:54 +0530

    Modified tests to cover ooniInstance.kill() without modifying ooniInstance.ooni directly

--- main/utils/ooni/__tests__/ooniprobe.test.js
+++ main/utils/ooni/__tests__/ooniprobe.test.js
@@ -35,28 +35,17 @@ describe('Test the initialization of Ooniprobe instances', () => {
   test('Ooniprobe instances are initialized in an expected way', async () => {
     const ooniInstance = new Ooniprobe()
     expect(ooniInstance._binaryPath).toBeTruthy()
-    expect(ooniInstance._binaryPath).toMatch(/amd64/i)
+    expect(ooniInstance._binaryPath).toMatch(/linux_amd64/i)
     expect(ooniInstance.ooni).toBeNull()
 
     // Checking if the kill and call functions are
     // defined in the initialized Ooniprobe instance
-    expect(ooniInstance.kill).toBeTruthy()
-    expect(ooniInstance.call).toBeTruthy()
+    expect(typeof ooniInstance.kill).toBe('function')
+    expect(typeof ooniInstance.call).toBe('function')
   })
 })
 
-describe('Tests if killing Ooniprobe instances work as expected', () => {
-  test('Killing Ooniprobe instance with truthy .ooni property calls stdin.end function', async () => {
-    const ooniInstance = new Ooniprobe()
-    ooniInstance.ooni = {
-      stdin: {
-        end: jest.fn(),
-      },
-    }
-    ooniInstance.kill()
-    expect(ooniInstance.ooni.stdin.end).toHaveBeenCalledTimes(1)
-  })
-
+describe('Killing Ooniprobe instance before calling .call() throws error', () => {
   test('Killing Ooniprobe instance if .ooni is null throws error', async () => {
     const ooniInstance = new Ooniprobe()
     const errorMessage = 'cannot kill an unstarted process'
@@ -112,6 +101,15 @@ describe('Ooniprobe instances invokes .call() method', () => {
       args.toString()
     )
     expect(childProcess.spawn.mock.calls[0][2]).toMatchObject(options)
+  })
+
+  test('Killing Ooniprobe instance with truthy .ooni property calls stdin.end function', async () => {
+    const ooniInstance = new Ooniprobe()
+
+    ooniInstance.call(['list'])
+    
+    ooniInstance.kill()
+    expect(ooniInstance.ooni.stdin.end).toHaveBeenCalledTimes(1)
   })
 })
 
